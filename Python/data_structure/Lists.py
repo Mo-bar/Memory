d569efe46e7760c1f021a8bc6ad9464fbe15c237
mourad_br <mouradbarkouch@gmail.com>
2022-05-29 01:53:43 +0100

    DS: list properties.

---
+++ Python/data_structure/Lists.py
@@ -0,0 +1,16 @@
+# -----------------------------
+# -- Lists --
+# -----------
+# [1] List Items Are Enclosed between  Square Brackets
+# [2] List Are Ordered
+# [3] List Are Mutable => Add, Delete, Edit
+# [4] List Items Is Not Unique
+# [5] List Can Have Different Data Types
+
+lst = ["One", "Two", "One", 1, 100.5, True]
+print('lst[0] ='+lst[0])
+print('lst[-1] ='+lst[-1])
+print('lst[-3] ='+lst[-3])
+print('lst[1:4] ='+lst[1:4])
+print('lst[:4] ='+lst[:4])
+print('lst[4:] ='+lst[4:])
