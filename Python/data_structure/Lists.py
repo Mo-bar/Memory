e20f7e5381b4c20462a0e527cb2893f604b3ecac
mourad_br <mouradbarkouch@gmail.com>
2022-05-29 02:20:28 +0100

    DS: list methods

--- Python/data_structure/Lists.py
+++ Python/data_structure/Lists.py
@@ -7,10 +7,31 @@
 # [4] List Items Is Not Unique
 # [5] List Can Have Different Data Types
 
+
+
+from tkinter.tix import Tree
+
+
 lst = ["One", "Two", "One", 1, 100.5, True]
 print('lst[0] ='+lst[0])
-print('lst[-1] ='+lst[-1])
-print('lst[-3] ='+lst[-3])
-print('lst[1:4] ='+lst[1:4])
-print('lst[:4] ='+lst[:4])
-print('lst[4:] ='+lst[4:])
+print('lst[-1] ='+str(lst[-1]))
+print('lst[-3] ='+str(lst[-3]))
+print(lst[1:4]) #['Two', 'One', 1]
+print(lst[:4]) # ['One', 'Two', 'One', 1]
+print(lst[4:]) # [100.5, True]
+
+print('='*50)
+#___Methods.
+friends = ['ali','mouad','simon']
+friends.append('yasmine') # add
+print(friends)
+nbr = [23,23,44]
+friends.append(nbr) # add list , friends list became multi demnsions
+print(friends[-1][2])
+friends.extend(nbr)  # friends += nbr
+print(friends)
+print('_'*50)
+friends.sort(reverse=Tree)
+friends.remove('mouad')
+nbr.reverse()
+print(nbr)
