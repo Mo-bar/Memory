a25004c0ca3c009321009af0a455dfd45bdba383
mourad_br <mouradbarkouch@gmail.com>
2022-08-05 21:42:28 +0100

    Data type collections.

---
+++ Dart/Collections/Datatype_collections.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  List<String> names = ['name1','name2'];  
+  List<Map<String,String>> persons = [
+    {
+      'nom' : 'Mourad',
+      'prenom' : 'BARKOUCH',
+      'age' : '25'
+    }
+  ];
+  }
